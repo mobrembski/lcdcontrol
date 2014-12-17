49d280b5e4dc5cfad546a223463824a51df9fa51
Michał Obrembski <byku@byku.com.pl>
2014-12-17 16:48:07 +0100

    Initial commit.

---
+++ lcdcontrol.c
@@ -0,0 +1,284 @@
+/*
+ * lcdcontrol.c - Application to change parameters of LCD2USB display.
+              Modified sources from:
+ *            http://www.harbaum.org/till/lcd2usb
+ */
+
+#include "lcdcontrol.h"
+
+int lcd_send(int request, int value, int index) {
+  if(usb_control_msg(handle, USB_TYPE_VENDOR, request, 
+          value, index, NULL, 0, 1000) < 0) {
+    fprintf(stderr, "USB request failed!\n");
+    return -1;
+  }
+  return 0;
+}
+
+void lcd_flush(void) {
+  int request, value, index;
+  
+  /* anything to flush? ignore request if not */
+  if (buffer_current_type == -1)
+    return;
+  
+  /* build request byte */
+  request = buffer_current_type | (buffer_current_fill - 1);
+  
+  /* fill value and index with buffer contents. endianess should IMHO not */
+  /* be a problem, since usb_control_msg() will handle this. */
+  value = buffer[0] | (buffer[1] << 8);
+  index = buffer[2] | (buffer[3] << 8);
+  
+  /* send current buffer contents */
+  lcd_send(request, value, index);
+  
+  /* buffer is now free again */
+  buffer_current_type = -1;
+  buffer_current_fill = 0;
+}
+
+void lcd_enqueue(int command_type, int value) {
+  if ((buffer_current_type >= 0) && (buffer_current_type != command_type))
+    lcd_flush();
+  
+  /* add new item to buffer */
+  buffer_current_type = command_type;
+  buffer[buffer_current_fill++] = value;
+  
+  /* flush buffer if it's full */
+  if (buffer_current_fill == BUFFER_MAX_CMD)
+    lcd_flush();
+}
+
+void lcd_command(const unsigned char ctrl, const unsigned char cmd) {
+  lcd_enqueue(LCD_CMD | ctrl, cmd);
+}
+
+void lcd_clear(void) {
+  lcd_command(LCD_BOTH, 0x01);    /* clear display */
+  lcd_command(LCD_BOTH, 0x03);    /* return home */
+}
+
+void lcd_home(void) {
+  lcd_command(LCD_BOTH, 0x03);    /* return home */
+}
+
+void lcd_write(const char *data) {
+  int ctrl = LCD_CTRL_0;
+  
+  while(*data) 
+    lcd_enqueue(LCD_DATA | ctrl, *data++);
+  
+  lcd_flush();
+}
+
+void lcd_echo(void) {
+  int i, nBytes, errors=0;
+  unsigned short val, ret;
+
+  for(i=0;i<ECHO_NUM;i++) {
+    val = rand() & 0xffff;
+    
+    nBytes = usb_control_msg(handle, 
+     USB_TYPE_VENDOR | USB_RECIP_DEVICE | USB_ENDPOINT_IN, 
+     LCD_ECHO, val, 0, (char*)&ret, sizeof(ret), 1000);
+
+    if(nBytes < 0) {
+      fprintf(stderr, "lcd_echo:\tUSB request failed!\n");
+      return;
+    }
+
+    if(val != ret)
+      errors++;
+  }
+
+  if(errors) 
+    fprintf(stderr, "ERROR: %d out of %d echo transfers failed!\n", 
+      errors, ECHO_NUM);
+  else 
+    if(verbose_mode)
+      printf("Echo test successful!\n");
+}
+
+int lcd_get(unsigned char cmd) {
+  unsigned char       buffer[2];
+  int                 nBytes;
+
+  /* send control request and accept return value */
+  nBytes = usb_control_msg(handle, 
+     USB_TYPE_VENDOR | USB_RECIP_DEVICE | USB_ENDPOINT_IN, 
+     cmd, 0, 0, (char *)buffer, sizeof(buffer), 1000);
+
+  if(nBytes < 0) {
+    fprintf(stderr, "lcd_get:\tUSB request failed!\n");
+    return -1;
+  }
+
+  return buffer[0] + 256*buffer[1];
+}
+
+void lcd_get_version(void) {
+  int ver = lcd_get(LCD_GET_FWVER);
+
+  if(ver != -1 && verbose_mode) 
+    printf("Firmware version %d.%d\n", ver&0xff, ver>>8);
+}
+
+void lcd_get_controller(void) {
+  int ctrl = lcd_get(LCD_GET_CTRL);
+
+  if(ctrl != -1 && verbose_mode) {
+    if(ctrl)
+      printf("Installed controllers: %s%s\n", 
+       (ctrl&1)?"CTRL0":"",
+       (ctrl&2)?" CTRL1":"");
+    else
+      fprintf(stderr, "No controllers installed!\n");
+  }
+}
+
+void lcd_get_keys(void) {
+  int keymask = lcd_get(LCD_GET_KEYS);
+
+  if(keymask != -1 && verbose_mode) 
+    printf("Keys: 0:%s 1:%s\n",
+     (keymask&1)?"on":"off",
+     (keymask&2)?"on":"off");
+}
+
+void lcd_set(unsigned char cmd, int value) {
+  if(usb_control_msg(handle, 
+       USB_TYPE_VENDOR, cmd, value, 0, 
+       NULL, 0, 1000) < 0) {
+    fprintf(stderr, "lcd_get_keys:\tUSB request failed!\n");
+  }
+}
+
+void lcd_fade_out(int time) {
+  int i;
+  for(i=255;i>=0;i--) {
+    lcd_set(LCD_SET_BRIGHTNESS, i);
+    MSLEEP(time);
+  }
+}
+
+void lcd_fade_in(int time) {
+  int i;
+  for(i=0;i<=255;i++) {
+    lcd_set(LCD_SET_BRIGHTNESS, i);
+    MSLEEP(time);
+  }
+}
+
+void parseArguments(struct arguments *arguments) {
+  if(arguments->contrast!=-1) {
+    lcd_set(LCD_SET_CONTRAST, arguments->contrast);
+  }
+  if(arguments->fade_out!=-1) {
+    lcd_fade_out(arguments->fade_out);
+  }
+  if(arguments->fade_in!=-1) {
+    lcd_fade_in(arguments->fade_in);
+  }
+  if(arguments->brightness!=-1) {
+    lcd_set(LCD_SET_BRIGHTNESS, arguments->brightness);
+  }
+}
+
+static error_t
+parse_opt (int key, char *arg, struct argp_state *state)
+{
+  struct arguments *arguments = state->input;
+  int val = 0;
+  if(arg)
+    sscanf(arg,"%d",&val);
+  switch (key)
+    {
+    case 'v':
+      verbose_mode=1;
+      break;
+    case 'c':
+      arguments->contrast=val;    
+      break;
+    case 'b':
+      arguments->brightness=val;
+      break;
+    case 'u':
+      arguments->fade_in=val;
+      break;
+    case 'd':
+      arguments->fade_out=val;
+      break;
+    case ARGP_KEY_ARG:
+      break;
+    case ARGP_KEY_END:
+      break;
+    default:
+      return ARGP_ERR_UNKNOWN;
+    }
+  return 0;
+}
+
+int main(int argc, char *argv[]) {
+  struct usb_bus      *bus;
+  struct usb_device   *dev;
+  struct arguments arguments;
+
+  /* Default values. */
+  arguments.fade_in = -1;
+  arguments.fade_out = -1;
+  arguments.contrast = -1;
+  arguments.brightness = -1;
+  argp_parse (&argp, argc, argv, 0, 0, &arguments);
+
+  usb_init();
+  
+  usb_find_busses();
+  usb_find_devices();
+  
+  for(bus = usb_get_busses(); bus; bus = bus->next) {
+    for(dev = bus->devices; dev; dev = dev->next) {
+      if((dev->descriptor.idVendor == LCD2USB_VID) && 
+   (dev->descriptor.idProduct == LCD2USB_PID)) {
+  
+  if(verbose_mode)
+   printf("Found LCD2USB device on bus %s device %s.\n", 
+         bus->dirname, dev->filename);
+  
+  /* open device */
+  if(!(handle = usb_open(dev))) 
+    fprintf(stderr, "Error: Cannot open USB device: %s\n", 
+      usb_strerror());
+  break;
+      }
+    }
+  }
+  
+  if(!handle) {
+    fprintf(stderr, "Error: Could not find LCD2USB device\n");
+
+#ifdef WIN
+    printf("Press return to quit\n");
+    getchar();
+#endif
+    exit(-1);
+  }
+
+  /* make lcd interface return some bytes to */
+  /* test transfer reliability */
+  lcd_echo();
+  /* read some values from adaptor */
+  lcd_get_version();
+  lcd_get_controller();
+  lcd_get_keys();
+  parseArguments(&arguments);
+  usb_close(handle);
+
+#ifdef WIN
+  printf("Press return to quit\n");
+  getchar();
+#endif
+
+  return 0;
+}
---
+++ lcdcontrol.h
@@ -0,0 +1,133 @@
+/*
+ * lcdcontrol.h - Application to change parameters of LCD2USB display.
+              Modified sources from:
+ *            http://www.harbaum.org/till/lcd2usb
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <usb.h>
+#include <argp.h>
+
+/* vendor and product id */
+#define LCD2USB_VID  0x0403
+#define LCD2USB_PID  0xc630
+
+/* target is a bit map for CMD/DATA */
+#define LCD_CTRL_0         (1<<3)
+#define LCD_CTRL_1         (1<<4)
+#define LCD_BOTH           (LCD_CTRL_0 | LCD_CTRL_1)
+
+#define LCD_ECHO           (0<<5)
+#define LCD_CMD            (1<<5)
+#define LCD_DATA           (2<<5)
+#define LCD_SET            (3<<5)
+#define LCD_GET            (4<<5)
+
+/* target is value to set */
+#define LCD_SET_CONTRAST   (LCD_SET | (0<<3))
+#define LCD_SET_BRIGHTNESS (LCD_SET | (1<<3))
+#define LCD_SET_RESERVED0  (LCD_SET | (2<<3))
+#define LCD_SET_RESERVED1  (LCD_SET | (3<<3))
+
+/* target is value to get */
+#define LCD_GET_FWVER      (LCD_GET | (0<<3))
+#define LCD_GET_KEYS       (LCD_GET | (1<<3))
+#define LCD_GET_CTRL       (LCD_GET | (2<<3))
+#define LCD_GET_RESERVED1  (LCD_GET | (3<<3))
+
+#ifdef WIN
+#include <windows.h>
+#include <winbase.h>
+#define MSLEEP(a) Sleep(a)
+#else
+#define MSLEEP(a) usleep(a*1000)
+#endif
+#define ECHO_NUM 100
+
+usb_dev_handle      *handle = NULL;
+
+/* to increase performance, a little buffer is being used to */
+/* collect command bytes of the same type before transmitting them */
+#define BUFFER_MAX_CMD 4        /* current protocol supports up to 4 bytes */
+int buffer_current_type = -1;   /* nothing in buffer yet */
+int buffer_current_fill = 0;    /* -"- */
+unsigned char buffer[BUFFER_MAX_CMD];
+/* command format:
+ * 7 6 5 4 3 2 1 0
+ * C C C T T R L L
+ *
+ * TT = target bit map
+ * R = reserved for future use, set to 0
+ * LL = number of bytes in transfer - 1
+ */
+/* ArgParse related stuff... */
+const char *argp_program_version =
+  "--      LCD2USB control application      --\n\
+  --      (c) 2014 by Michal Obrembski     --\n\
+  --  Modified sources from Till Harbaum   --\n\
+  -- http://www.harbaum.org/till/lcd2usb   --\n";
+const char *argp_program_bug_address =
+  "<byku@byku.com.pl>";
+
+/* Program documentation. */
+static char doc[] =
+  "lcdcontrol -- program to control display connected via LCD2USB.";
+/* ArgParse handler */
+static error_t
+parse_opt (int key, char *arg, struct argp_state *state);
+/* A description of the arguments we accept. */
+static struct argp_option options[] = {
+    {"verbose",'v',0,0,"Produce verbose output." },
+    {"contrast",'c',"VALUE",0,"Set static contrast." },
+    {"brightness",'b',"VALUE",0,"Set static brightness." },
+    {"fade-in",'u',"TIME",0,"Fade-in brightness with given time." },
+    {"fade-out",'d',"TIME",0,"Fade-out brightness with given time." },
+    { 0 }
+};
+struct arguments
+{
+  int contrast;
+  int brightness;
+  int fade_in;
+  int fade_out;
+};
+static struct argp argp = { options, parse_opt, 0, doc };
+static int verbose_mode = 0;
+/* flush command queue due to buffer overflow / content */
+/* change or due to explicit request */
+void lcd_flush(void);
+/* enqueue a command into the buffer */
+void lcd_enqueue(int command_type, int value);
+/* see HD44780 datasheet for a command description */
+void lcd_command(const unsigned char ctrl, const unsigned char cmd);
+/* clear display */
+void lcd_clear(void);
+/* return cursor to home */
+void lcd_home(void);
+/* write a data string to the first display */
+void lcd_write(const char *data);
+/* send a number of 16 bit words to the lcd2usb interface */
+/* and verify that they are correctly returned by the echo */
+/* command. This may be used to check the reliability of */
+/* the usb interfacing */
+void lcd_echo(void);
+/* get a value from the lcd2usb interface */
+int lcd_get(unsigned char cmd);
+/* get lcd2usb interface firmware version */
+void lcd_get_version(void);
+/* get the bit mask of installed LCD controllers (0 = no */
+/* lcd found, 1 = single controller display, 3 = dual */
+/* controller display */
+void lcd_get_controller(void);
+/* get state of the two optional buttons */
+void lcd_get_keys(void);
+/* set a value in the LCD interface */
+void lcd_set(unsigned char cmd, int value);
+/* fade out LCD brightness with TIME steps */
+void lcd_fade_out(int time);
+/* fade in LCD brightness with TIME steps */
+void lcd_fade_in(int time);
+/* Parses arguments received from user */
+void parseArguments(struct arguments *arguments);
